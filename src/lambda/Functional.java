349080cea00c62aa077325d7ef17cda4f23399f3
Jeromy0515 <qkr07224@gmail.com>
2021-05-10 19:49:49 +0900

    :)

--- src/lambda/Functional.java
+++ src/lambda/Functional.java
@@ -5,5 +5,5 @@ public interface Functional {
 	
 	public boolean equals(Object obj); // Object 클래스에 있는 메서드는 인터페이스에 선언해도 Functional Interface에 지장을 주지 않음
 	public void execute(); // Object 클래스의  메소드를 제외하고 하나의 추상 메서드만 선언되어 있는 경우 Functional Interface 임
-	
+	 
 }
